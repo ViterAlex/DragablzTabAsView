51aed1d0f6118f994513324a3b6113023ee997d4
Александр Витер <viter.alex@gmail.com>
2017-03-25 18:36:04 +0200

    Динамическое создание вкладок

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
 using DragablzTabAsView.Commands;
+using DragablzTabAsView.Views;
 
 namespace DragablzTabAsView.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+
+        public ObservableCollection<ViewModelBase> Views { get; set; }
+
+        private DelegateCommand<int> _addViewCommand;
+
+        public ICommand AddViewCommand
+        {
+            get
+            {
+                if (_addViewCommand == null)
+                {
+                    _addViewCommand = new DelegateCommand<int>(AddView);
+                }
+                return _addViewCommand;
+            }
+        }
+
+        private void AddView(int i)
+        {
+            if (Views == null)
+            {
+                Views = new ObservableCollection<ViewModelBase>();
+            }
+            switch (i)
+            {
+                case 1:
+                    Views.Add(new FirstViewModel());
+                    break;
+                case 2:
+                    Views.Add(new SecondViewModel());
+                    break;
+                case 3:
+                    Views.Add(new ThirdViewModel());
+                    break;
+            }
+            OnPropertyChanged("Views");
+        }
 
         #region Constructor
 

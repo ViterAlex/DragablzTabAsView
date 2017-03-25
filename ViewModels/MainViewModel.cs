3a65a4d8e426fa1ac2fbf8dd372a88941259e869
Александр Витер <viter.alex@gmail.com>
2017-03-25 12:59:16 +0200

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+using DragablzTabAsView.Commands;
+
+namespace DragablzTabAsView.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            // Blank
+        }
+
+        #endregion
+
+    }
+}
